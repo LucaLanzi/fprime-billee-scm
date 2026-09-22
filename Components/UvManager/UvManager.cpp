567c9f7069bb5524f43a2c8156b70ccc74f76daa
Vernonicuhhh <Vernonicuhhh@protonmail.com>
2026-09-22 01:58:08 +0000

    feat: add telemetry + human-readable log msg

--- Components/UvManager/UvManager.cpp
+++ Components/UvManager/UvManager.cpp
@@ -29,12 +29,23 @@ void UvManager ::run_handler(FwIndexType portNum, U32 context) {
 // ----------------------------------------------------------------------
 
 void UvManager ::uvToggle_cmdHandler(FwOpcodeType opCode, U32 cmdSeq, Fw::On toggle) {
+    Fw::Enabled relayState;
     Fw::Logic logicLvl = toggle == Fw::On::ON ? Fw::Logic::HIGH : Fw::Logic::LOW;
-    Fw::Enabled uvEnabled = toggle == Fw::On::ON ? Fw::Enabled::ENABLED : Fw::Enabled::DISABLED;
 
-    this->uvSet_out(0, logicLvl);
-    this->tlmWrite_uvState(uvEnabled);
-    this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
+    Drv::GpioStatus writeOk = this->uvSet_out(0, logicLvl);
+
+    if(writeOk == Drv::GpioStatus::OP_OK){
+        this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
+        relayState = toggle == Fw::On::ON ? Fw::Enabled::ENABLED : Fw::Enabled::DISABLED;
+    }
+    else{
+        this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::EXECUTION_ERROR);
+        relayState = toggle == Fw::On::ON ? Fw::Enabled::DISABLED : Fw::Enabled::ENABLED;
+    }
+    this->tlmWrite_uvState(relayState);
+    this->log_ACTIVITY_HI_uvStateChange(relayState);
+
+
 }
 
 }  // namespace billeeScm
